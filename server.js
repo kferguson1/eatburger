37b5587e55b0d9ecbd5cddcfcaad44678c1604f5
Kiara Ferguson <Kiaraferguson16@yahoo.com>
2021-03-06 09:43:01 -0500

    express change

--- server.js
+++ server.js
@@ -1,39 +1,68 @@
-//Dependencies
+// //Dependencies
+
+// var express = require("express");
+
+
+// var PORT = process.env.PORT || 8080;
+// var app = express();
+
+// //Serve static content for app from "public" directory
+
+// app.use(express.static('public'));
+
+// //Parse application/urlencoded
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// //Parse application/json
+
+// app.use(bodyParser.json());
+
+// //Set Handlebars
+
+// var exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// // Import routes
+
+// var routes = require("./controllers/burgerController");
+
+// app.use(routes);
+
+// //Listen function
+
+// app.listen(PORT, function() {
+//   console.log("App now listening at localhost:" + PORT);
+// });
 
 var express = require("express");
-var bodyParser = require("body-parser");
-
 
 var PORT = process.env.PORT || 8080;
+
 var app = express();
 
-//Serve static content for app from "public" directory
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-app.use(express.static('public'));
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//Parse application/urlencoded
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//Parse application/json
-
-app.use(bodyParser.json());
-
-//Set Handlebars
-
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes
-
-var routes = require("./controllers/burgerController");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgerController.js");
 
 app.use(routes);
 
-//Listen function
-
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
